831d213a2720ef557705b011505b0bdbb2b707b2
Policycin <631637682@qq.com>
2016-10-29 22:59:45 +0800

    The first test case!

---
+++ src/com/fra/solve.java
@@ -0,0 +1,24 @@
+package com.fra;
+
+public class solve {
+
+	/**
+	 * @param args
+	 */
+	public static int MaxSubstring(String str){
+		int Mlength=0;
+		if(str.length()==0){
+			return Mlength;
+		}
+		int cur=0;
+		
+		return Mlength;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="";
+		System.out.print("最大不重复字符串的长度是：");
+		System.out.println(MaxSubstring(str));
+
+	}
+}
