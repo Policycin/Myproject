0e5923900dd04f0ee0e15afa4d3de0f6c0619839
Policycin <631637682@qq.com>
2016-10-29 23:22:52 +0800

    the second case

--- src/com/fra/solve.java
+++ src/com/fra/solve.java
@@ -1,5 +1,5 @@
 package com.fra;
-
+import java.util.*;
 public class solve {
 
 	/**
@@ -7,16 +7,15 @@ public class solve {
 	 */
 	public static int MaxSubstring(String str){
 		int Mlength=0;
-		if(str.length()==0){
+		if(str.length()==0||str.length()==1){
+			Mlength=str.length();
 			return Mlength;
 		}
-		int cur=0;
-		
 		return Mlength;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String str="";
+		String str="a";
 		System.out.print("最大不重复字符串的长度是：");
 		System.out.println(MaxSubstring(str));
 
